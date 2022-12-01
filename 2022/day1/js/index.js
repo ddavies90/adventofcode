837a266879120ddf5ce2f0d7b4c97d378be96501
David Davies <david.davies@theverygroup.com>
2022-12-01 17:54:44 +0000

    Completed day 1 js

---
+++ 2022/day1/js/index.js
@@ -0,0 +1,35 @@
+import { readFileSync } from "fs";
+
+const getSortedElfCalorieArray = (file) => {
+  return file
+    .trim()
+    .split("\n\n")
+    .map((value) =>
+      value
+        .split("\n")
+        .map((value) => Number(value))
+        .reduce((acc, val) => acc + val)
+    );
+};
+
+const calculateMostCalories = (array) => {
+  const result = array.sort((a, b) => b - a)[0];
+  console.log(result);
+  return result;
+};
+
+const calculateTopThree = (array) => {
+  const result = array
+    .sort((a, b) => b - a)
+    .splice(0, 3)
+    .reduce((acc, curr) => acc + curr);
+  console.log(result);
+  return result;
+};
+
+const fileContents = readFileSync("input.txt", "utf-8");
+
+const sortedElfArray = getSortedElfCalorieArray(fileContents);
+
+calculateMostCalories(sortedElfArray);
+calculateTopThree(sortedElfArray);
